60269ec2bcbe90a515882aef36d13b869fc234ef
ShroukOuda <s36568961@gmail.com>
2024-10-04 12:06:24 +0300

    add line before function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,14 +2,15 @@
 """
     pascal traingle
 """
+
 def factorial_iterative(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
 
-traingle_list = []
 def pascal_triangle(rows):
+    traingle_list = []
     for n in range(rows):
         row_list = []
         for r in range(n + 1):
