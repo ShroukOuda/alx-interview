08efac2546edd6ea886f45a040bf9c25bbafdae9
ShroukOuda <s36568961@gmail.com>
2024-10-04 11:59:49 +0300

    add factorial_iterative function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-from math import factorial
 """
     pascal traingle
 """
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
 traingle_list = []
 def pascal_triangle(rows):
     for n in range(rows):
         row_list = []
         for r in range(n + 1):
-            nCr = factorial(n) // (factorial(n - r) * factorial(r))
+            nCr = factorial_iterative(n) // (factorial_iterative(n - r) * factorial_iterative(r))
             row_list.append(nCr)
         traingle_list.append(row_list)
     return traingle_list
